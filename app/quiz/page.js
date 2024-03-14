61ab1f985a971809c1b3ee5f2bdb5c5896034ea4
Negar <n.arasterad@gmail.com>
2024-03-14 23:10:22 +0330

    VercelError

--- app/quiz/page.js
+++ app/quiz/page.js
@@ -102,6 +102,7 @@ const page = () => {
                     >
                       
                       {answer}
+                      
                     </li>
                   );
                 })}
